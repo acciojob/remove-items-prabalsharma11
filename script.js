d1dc446b4c9b698845a177d8ce6f5fc1367dd348
Prabal Raj Sharma <109718530+prabalsharma11@users.noreply.github.com>
2024-11-14 23:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const slc = document.getElementById('colorSelect')
+      const btn = document.querySelector('input[type="button"]')
+      btn.addEventListener('click', () => {
+        const opt = slc.options[slc.selectedIndex]
+        if (opt) {
+          slc.removeChild(opt)
+        }
+      })
